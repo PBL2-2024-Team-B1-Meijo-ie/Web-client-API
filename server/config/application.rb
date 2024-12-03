885d7a025afb6c5e0c9eafceff69eb861033f327
Ryousuke <115251760+Ryousuke@users.noreply.github.com>
2024-12-03 13:46:21 +0900

    testdb変更

--- server/config/application.rb
+++ server/config/application.rb
@@ -22,6 +22,13 @@ module Server
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
+
+    # Middleware for sessions and cookies
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_your_app_session'
+
+    # Ensure session store configuration
+    config.session_store :cookie_store, key: '_your_app_session'
   end
 end
