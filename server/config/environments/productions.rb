ec8aa4ff5934f09a999858280038a9b9dc5803c1
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2024-12-19 15:00:18 +0900

    update: production.rb (#35)
    本番環境でメール送信ができるように，使用するポートを変えた
    これでどうにか動いてほしい

--- server/config/environments/productions.rb
+++ server/config/environments/productions.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
-
+  
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
@@ -76,7 +76,14 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
   # メールを扱うためのもの
-  config.action_mailer.delivery_method = :resend
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address   => 'smtp.resend.com',
+    :port      => 465,
+    :user_name => 'resend',
+    :password  => ENV['RESEND_API_KEY'],
+    :tls => true
+  }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
